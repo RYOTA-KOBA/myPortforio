e36c4357bd478686c80913505565cab7c2b1928a
RYOTA <kobaryo04ysh@icloud.com>
2020-10-09 01:17:43 +0900

    add hamburger menu

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ let scroll = new SmoothScroll('a[href*="#"]', {
     header: '[data-scroll-header]',
     speed: 1500,
 });
+
+// ハンバーガーメニュー
+function hamburger() {
+    document.querySelector('body').classList.toggle('menu-open');
+}
+document.querySelector('.mobile-menu__btn').addEventListener('click' , function () {
+    hamburger();
+} );
+
+function overRay() {
+    document.querySelector('body').classList.remove('menu-open');
+}
+document.querySelector('.mobile-menu__cover').addEventListener('click' , function () {
+    overRay();
+} );
+
+// const menuOpen = document.querySelectorAll('.mobile-menu__btn, .mobile-menu__cover');
+
+// menuOpen.forEach(function() {
+//     hamburger();
+// });
